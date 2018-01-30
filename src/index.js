d31bbfd3b00d3c1be29031e1972a07e7f2fe2bb0
David Chan <sediwen@qq.com>
2018-01-30 18:20:52 +0800

    欢迎光临

---
+++ src/index.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares:Array(9).fill(null),
+    }
+  }
+
+  handleClick(i) {
+   const squares = this.state.squares.slice(); //直接赋值也可以啊，为什么要用浅拷贝呢？
+   squares[i] = 'X';
+   this.setState({squares: squares});
+ }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+// 一个组件只能返回一个元素，所以下面注释掉了其它元素。什么？不懂什么是组件，简单来说，组件是由很多个元素构成的，组件是由class定义的。
+
+ReactDOM.render(
+  <Game />,
+  //<ShoppingList name="David" />,
+  document.getElementById('root')
+);
